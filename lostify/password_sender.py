ea49d6eb5d767dbb633ebead47003fc1e1f6913a
anirudh-bijay <190929747+anirudh-bijay@users.noreply.github.com>
2025-04-01 18:53:14 +0530

    Add module for sending randomly generated password on request to reset password

---
+++ lostify/password_sender.py
@@ -0,0 +1,48 @@
+from azure.communication.email import EmailClient
+from dotenv import dotenv_values
+
+POLLER_WAIT_TIME = 10   # seconds
+
+def send_password(password: int, email: str, recipient_display_name: str):
+    try:
+        env = dotenv_values(".env", verbose = True)
+
+        email_client = EmailClient.from_connection_string(env['AZURE_CONNECTION_STRING'])
+
+        message = {
+            "content": {
+                "subject": "Test Email",
+                "plainText": f"LOSTIFY\n----------\nYour new password is: {password}\nPlease change this password after login.",
+                "html": f"<html><h1>Lostify</h1><p>Your new password is: <code>{password}</code><p>Please change this password after login.</html>"
+            },
+            "recipients": {
+                "to": [
+                    {
+                        "address": email,
+                        "displayName": recipient_display_name
+                    }
+                ]
+            },
+            "senderAddress": env['AZURE_SENDER_EMAIL']
+        }
+
+        poller = email_client.begin_send(message)
+
+        time_elapsed = 0
+        while not poller.done():
+            print("Email send poller status: " + poller.status())
+
+            poller.wait(POLLER_WAIT_TIME)
+            time_elapsed += POLLER_WAIT_TIME
+
+            if time_elapsed > 6 * POLLER_WAIT_TIME:
+                raise TimeoutError("Polling timed out.")
+
+        if poller.result()["status"] == "Succeeded":
+            print(f"Successfully sent the email (operation id: {poller.result()['id']})")
+        else:
+            raise RuntimeError(str(poller.result()["error"]))
+    except Exception as ex:
+        print('Exception: ')
+        print(ex)
+        raise ex
